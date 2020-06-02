009b82d7b4bd697daa590dbcd04067966966f5dd
Lin, Ming Chih <mingchih.lin8@gmail.com>
2020-06-02 13:38:34 +0800

    Update getDesignProperties.py

--- 程式碼範例/取得Design的屬性/getDesignProperties.py
+++ 程式碼範例/取得Design的屬性/getDesignProperties.py
@@ -1,5 +1,8 @@
 oProject = oDesktop.GetActiveProject()
 oDesign = oProject.GetActiveDesign()
+oEditor = oDesign.GetActiveEditor()
 
 p = oDesign.GetProperties("DefinitionParameterTab", "Instance:{}".format(oDesign.GetName()))
-AddWarningMessage(str(p))
+for i in p:
+    v = oEditor.GetPropertyValue("DefinitionParameterTab", "Instance:{}".format(oDesign.GetName()), i)
+    AddWarningMessage("{}:{}".format(i, v))
