c838c03afacbc7e49d3670addf17828bd1c65299
Lin, Ming Chih <mingchih.lin8@gmail.com>
2020-06-16 15:32:06 +0800

    Create writeExcel.py

---
+++ 程式碼範例/寫入EXCEL檔案/writeExcel.py
@@ -0,0 +1,10 @@
+import clr
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+
+import Microsoft.Office.Interop.Excel as Excel
+excel = Excel.ApplicationClass()
+excel.Visible = True
+
+workbook = excel.Workbooks.Add()
+ws = workbook.Worksheets.Add()
+ws.Range["A1"]= "Hello World"
