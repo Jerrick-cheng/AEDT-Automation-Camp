6fc23755692472d6c38123e824e2c50cb31923e5
Lin, Ming Chih <mingchih.lin8@gmail.com>
2020-06-16 15:52:58 +0800

    Update writeExcel.py

--- 程式碼範例/寫入EXCEL檔案/writeExcel.py
+++ 程式碼範例/寫入EXCEL檔案/writeExcel.py
@@ -1,3 +1,5 @@
+#color index: https://docs.microsoft.com/zh-tw/office/vba/api/excel.colorindex
+
 import clr
 clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 
@@ -8,3 +10,6 @@ excel.Visible = True
 workbook = excel.Workbooks.Add()
 ws = workbook.Worksheets.Add()
 ws.Range["A1"]= "Hello World"
+ws.Range["A1"].Interior.ColorIndex = 4
+usedrange = ws.UsedRange
+usedrange.Columns.AutoFit()
